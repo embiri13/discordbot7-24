56aa563176da4521c74ecf8ddb1d592013d6e4e2
embiri13 <43474169+embiri13@users.noreply.github.com>
2018-09-21 17:28:44 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,7 +14,7 @@ const log = message => {
 };
 
 client.on("ready", () => {
-  client.user.setGame(prefix + "öneri");
+  client.user.setGame(prefix + "yardım !öneri");
   console.log("Bağlandım!")   
 });
 

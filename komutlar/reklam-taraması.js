db8fd1ec556dc283e8ee519927e8ee0e0fe66f9f
embiri13 <43474169+embiri13@users.noreply.github.com>
2018-09-23 18:09:52 +0300

    Add files via upload

---
+++ komutlar/reklam-taraması.js
@@ -0,0 +1,19 @@
+const Discord = require('discord.js');
+
+module.exports.run = (client, msg, args) => {
+ const members = msg.guild.members.filter(member => member.user.presence.game && /(discord\.(gg|io|me|li)\/.+|discordapp\.com\/invite\/.+)/i.test(member.user.presence.game.name));
+ return msg.channel.send(members.map(member => `\`${member.id}\` ${member.displayName}`).join("\n") || "Kimsenin oynuyor mesajı reklam içermiyor.");
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['reklam-taraması'],
+  permLevel: "0"
+};
+
+exports.help = {
+  name: "reklam-taraması",
+  description: "oynuyorda reklamlara bakar",
+  usage: "reklam-taraması"
+};

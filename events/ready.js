d9515f57b9799764fcc2c9fea068556ac87db6cf
embiri13 <43474169+embiri13@users.noreply.github.com>
2018-10-09 17:20:36 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -3,14 +3,12 @@ const moment = require('moment');
 const Discord = require('discord.js');
 const ayarlar = require('../ayarlar.json');
 
-var oyun = [
-        "client.user.setGame(prefix + "yardım |Bot Artık Hostinge Bağlı");",
-        "client.user.setGame(prefix + "yardım | " + client.guilds.size + " Sunucu " + client.users.size + " Kullanıcı ", );"
-    ];
+var prefix = ayarlar.prefix;
 
-    setInterval(function() {
-
-        var random = Math.floor(Math.random()*(oyun.length-0+1)+0);
-
-        client.user.setGame(oyun[random], "https://www.twitch.tv/croxydad");
-        }, 2 * 2500);
+module.exports = client => {
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Aktif, Komutlar yüklendi!`);
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı!`);
+  client.user.setStatus("online");
+  client.user.setGame(`..yardım|Türkiye`);
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Oyun ismi ayarlandı!`);
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Şu an ` + client.channels.size + ` adet kanala, ` + client.guilds.size + ` adet sunucuya ve ` + client.guilds.
